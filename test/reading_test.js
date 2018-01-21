859dcf47db04e86054890d0f18c091f721a59c18
a-m-a-z <ahmad.abdulaziz37@gmail.com>
2018-01-21 12:36:03 +0100

    add more test to retrivee data form db

--- test/reading_test.js
+++ test/reading_test.js
@@ -1,21 +1,34 @@
 const assert = require('assert');
 const User = require('../src/user');
 
-describe('Getting User for database',()=>{
+describe('Getting User for database', () => {
 	let joe;
 
-	beforeEach((done)=>{
-		joe = new User({ name: 'joe'})
+	beforeEach((done) => {
+		joe = new User({
+			name: 'joe'
+		})
 		joe.save()
-			.then(()=>done())
+			.then(() => done())
 	})
 
-	it('Getting all users with name \'joe\'', (done)=>{
-		User.find({ name: 'Joe'})
-			.then((users)=>{
-				console.log(users);
+	it('Getting all users with name \'joe\'', (done) => {
+		User.find({
+				name: 'joe'
+			})
+			.then((users) => {
+				assert(users[0]._id.toString() === joe._id.toString());
+				done();
+			})
+	})
+
+	it('Getting a particular user', (done) => {
+		User.findOne({
+				_id: joe._id
+			})
+			.then((user) => {
+				assert(user.name === 'joe');
 				done();
 			})
 	})
 })
-	
