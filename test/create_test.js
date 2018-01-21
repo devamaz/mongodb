1ab1805d53ec4fd6ce4a9f39e41346b63da684bc
a-m-a-z <ahmad.abdulaziz37@gmail.com>
2018-01-21 11:35:26 +0100

    testing create date

--- test/create_test.js
+++ test/create_test.js
@@ -2,15 +2,16 @@ const assert = require('assert');
 const User = require('../src/user');
 
 
-describe('creating Record', ()=>{
-    it('save user', (done)=>{
-        const joe = new User({ name: 'Joe'});
-        
+describe('creating Record', () => {
+    it('save user', (done) => {
+        const joe = new User({
+            name: 'Joe'
+        });
+
         joe.save()
-            .then(()=>{
+            .then(() => {
                 assert(!joe.isNew);
                 done();
             })
-        
     });
 });
