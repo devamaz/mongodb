04e928754d2193594888a565cbc9e2ca64295ced
a-m-a-z <ahmad.abdulaziz37@gmail.com>
2018-01-21 11:36:22 +0100

    testing retrivee data form db

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,6 +2,20 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Getting User for database',()=>{
-	
+	let joe;
+
+	beforeEach((done)=>{
+		joe = new User({ name: 'joe'})
+		joe.save()
+			.then(()=>done())
+	})
+
+	it('Getting all users with name \'joe\'', (done)=>{
+		User.find({ name: 'Joe'})
+			.then((users)=>{
+				console.log(users);
+				done();
+			})
+	})
 })
 	
